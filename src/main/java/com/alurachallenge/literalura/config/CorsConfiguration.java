599f6476795edf6ae4c273f3b05f1c9d716fcd40
dear-alina <haku16012@gmail.com>
2026-07-14 11:45:07 -0500

    "CORS para Render"

--- src/main/java/com/alurachallenge/literalura/config/CorsConfiguration.java
+++ src/main/java/com/alurachallenge/literalura/config/CorsConfiguration.java
@@ -13,7 +13,7 @@ public class CorsConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500")
+                        .allowedOrigins("https://literalura-front.onrender.com","http://127.0.0.1:5500", "http://localhost:5500")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                         .allowedHeaders("*");
             }
