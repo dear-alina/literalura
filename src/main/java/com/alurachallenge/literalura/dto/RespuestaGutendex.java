e82b9aa06f1bdc708b6dfcb04e6b294c06cc6070
dear-alina <haku16012@gmail.com>
2026-07-15 22:44:04 -0500

    Actualización de RespuestaGutendex

--- src/main/java/com/alurachallenge/literalura/dto/RespuestaGutendex.java
+++ src/main/java/com/alurachallenge/literalura/dto/RespuestaGutendex.java
@@ -1,8 +1,11 @@
 package com.alurachallenge.literalura.dto;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record RespuestaGutendex(
     @JsonAlias("results")
     List<DatosGutendexLibro> resultados
